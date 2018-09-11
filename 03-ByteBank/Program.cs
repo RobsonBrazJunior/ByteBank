7024ceeb0762a09b9bb74b7aa0f7f5d3cf369998
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-11 15:49:27 -0400

    Verificando variaveis de referência de objetos.

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente();
+            contaCorrente.titular = "Nome titular";
+            contaCorrente.agencia = 863;
+            contaCorrente.numero = 863452;
+
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+            segundaContaCorrente.titular = "Nome titular";
+            segundaContaCorrente.agencia = 863;
+            segundaContaCorrente.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaCorrente == segundaContaCorrente));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeMaisUmaVez));
+
+            contaCorrente = segundaContaCorrente;
+            Console.WriteLine(contaCorrente == segundaContaCorrente);
+
+            contaCorrente.saldo = 300;
+            Console.WriteLine(contaCorrente);
+            Console.WriteLine(segundaContaCorrente);
+
+            Console.ReadLine();
+        }
+    }
+}
