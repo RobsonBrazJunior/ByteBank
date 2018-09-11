b6c8ccce3e6b4c3e89cc7198a378e27790bd5fa1
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-11 14:30:29 -0400

    Criando e populando obejto do tipo ContaCorrente

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,7 +10,22 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
-            new ContaCorrente();
+            ContaCorrente contaCliente01 = new ContaCorrente();
+
+            contaCliente01.titular = "Cliente01";
+            contaCliente01.agencia = 863;
+            contaCliente01.numero = 863452;
+            contaCliente01.saldo = 100;
+
+            Console.WriteLine(contaCliente01.titular);
+            Console.WriteLine("Agência: " + contaCliente01.agencia);
+            Console.WriteLine("Número: " + contaCliente01.numero);
+            Console.WriteLine("Saldo: " + contaCliente01.saldo);
+
+            contaCliente01.saldo += 200;
+            Console.WriteLine("Saldo: " + contaCliente01.saldo);
+
+            Console.ReadLine();
         }
     }
 }
