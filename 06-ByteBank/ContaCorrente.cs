f1897784afd045ffbc057198e1605d050ca2fc8b
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-17 17:08:17 -0400

    Padrão das propriedades Get e Set

--- 06-ByteBank/ContaCorrente.cs
+++ 06-ByteBank/ContaCorrente.cs
@@ -2,51 +2,35 @@
 {
     public class ContaCorrente
     {
-        public Cliente titular;
-        public int agencia;
-        public int numero;
-        private double _saldo = 100;
-
-        public double Saldo
-        {
-            get
-            {
-                return _saldo;
-            }
-
-            set
-            {
-                if (value < 0)
-                    return;
-
-                _saldo = value;
-            }
-        }
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
 
         public bool Sacar(double valor)
         {
-            if (this._saldo < valor)
+            if (this.Saldo < valor)
             {
                 return false;
             }
 
-            this._saldo -= valor;
+            this.Saldo -= valor;
             return true;
         }
 
         public void Depositar(double valor)
         {
-            this._saldo += valor;
+            this.Saldo += valor;
         }
 
         public bool Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (this._saldo < valor)
+            if (this.Saldo < valor)
             {
                 return false;
             }
 
-            this._saldo -= valor;
+            this.Saldo -= valor;
             contaDestino.Depositar(valor);
             return true;
         }
