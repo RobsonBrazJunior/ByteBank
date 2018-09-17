265fdb9d1f4ba3e3f4f1bdfaef76fc9750de2972
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-17 16:43:20 -0400

    DefinirSaldo e ObterSaldo

---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente();
+
+            contaCorrente.DefinirSaldo(10);
+
+            Console.WriteLine(contaCorrente.ObterSaldo());
+
+            Console.ReadLine();
+        }
+    }
+}
