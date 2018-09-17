a5926474d734edd7d3d4c9a59bd6781e3e04bd90
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-17 14:57:16 -0400

    Entendo Composição de Classes

--- 05-ByteBank/Program.cs
+++ 05-ByteBank/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace _05_ByteBank
+﻿using System;
+
+namespace _05_ByteBank
 {
     class Program
     {
         static void Main(string[] args)
         {
             Cliente cliente = new Cliente();
+
+            cliente.nome = "Nome do cliente";
+            cliente.profissao = "Profissão do cliente";
+            cliente.cpf = "CPF do cliente";
+
+            ContaCorrente contaCorrente = new ContaCorrente();
+            contaCorrente.titular = cliente;
+            contaCorrente.saldo = 500;
+            contaCorrente.agencia = 555;
+            contaCorrente.numero = 5634527;
+
+            Console.WriteLine(cliente.nome);
+            Console.WriteLine(contaCorrente.titular.nome);
+
+            Console.ReadLine();
         }
     }
 }
