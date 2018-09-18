ff637ea630fd303d129ed334ed28c46e8d747783
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-18 09:01:11 -0400

    Declarando Construtor na Classe ContaCorrente

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,58 @@
+﻿namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)                
+                    return;
+
+                _agencia = value;
+            }
+        }
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+
+        public ContaCorrente (int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this.Saldo < valor)
+            {
+                return false;
+            }
+
+            this.Saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.Saldo < valor)
+            {
+                return false;
+            }
+
+            this.Saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente(852, 897564213);
+
+            Console.WriteLine(contaCorrente.Agencia);
+            Console.WriteLine(contaCorrente.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
